b7cb1b4d34c141c7784f9f90ff3e7aa8b0e1900b
Plamen Paskov <paskov@gmail.com>
2015-07-23 20:30:58 +0300

    remove else clause

--- src/Battleships/Model/Game/Battleships/StrikeAction.php
+++ src/Battleships/Model/Game/Battleships/StrikeAction.php
@@ -22,17 +22,17 @@ class StrikeAction implements Action
 
     public function perform()
     {
+        $success = false;
+        $data = [
+            'message' => 'Miss'
+        ];
+
         if ($this->board->isShip($this->row, $this->col)) {
+            $success = true;
             $data = [
                 'shipId' => $this->board->get($this->row, $this->col),
                 'message' => 'Hit'
             ];
-            $success = true;
-        } else {
-            $data = [
-                'message' => 'Miss'
-            ];
-            $success = false;
         }
 
         $this->board->mark($this->row, $this->col);
